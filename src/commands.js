b14df9f0f6f4da9e41acf1422ebb81d323a82ea5
Shazron Abdullah <shazron@apache.org>
2014-11-26 16:17:28 -0800

    Completed "launch" and "start" commands. Incremented simctl version dependency to 0.0.5

--- src/commands.js
+++ src/commands.js
@@ -25,8 +25,103 @@ THE SOFTWARE.
 var path = require('path'),
     fs = require('fs'),
     help = require('./help'),
+    util = require('util'),
     simctl,
     bplist;
+
+
+function processDeviceTypeId(devicetypeid) {
+    // the object to return
+    var ret_obj = {
+        name : null,
+        id : null,
+        runtime : null
+    };
+    
+    // get the deviceid from --devicetypeid
+    // --devicetypeid is a string in the form "devicetype, runtime_version" (optional: runtime_version)
+    var arr = devicetypeid.split(',');
+    if (arr.length < 1) {
+      console.error('--devicetypeid was not specified.');
+      process.exit(1);
+    }
+
+    var devicetype = arr[0].trim();
+    
+    if (arr.length > 1) {
+        ret_obj.runtime = arr[1].trim();
+    }
+    
+    // now find the devicename from the devicetype
+    var options = { 'silent': true };
+    var list = simctl.list(options).json;
+    
+    for (var i=0; i < list.devicetypes.length; ++i) {
+        if (list.devicetypes[i].id === devicetype) {
+            ret_obj.name = list.devicetypes[i].name;
+        }
+    }
+    
+    // device name not found, exit
+    if (!ret_obj.name) {
+      console.error(util.format('Device type "%s" could not be found.', devicetype));
+      process.exit(1);
+    }
+    
+    // if runtime_version was not specified, we use a default. Use first available
+    var check_runtime_availability = true;
+    
+    if (!ret_obj.runtime) {
+        for (var i=0; i < list.runtimes.length; ++i) {
+            if (list.runtimes[i].available) {
+                ret_obj.runtime = list.runtimes[i].name;
+                check_runtime_availability = false;
+                break;
+            }
+        }
+    }
+    
+    // prepend iOS to runtime version, if necessary
+    if (ret_obj.runtime.indexOf('iOS') === -1) {
+        ret_obj.runtime = util.format('iOS %s', ret_obj.runtime);
+    }
+
+    var runtime_found = !check_runtime_availability;
+    
+    if (check_runtime_availability) {
+        for (var i=0; i < list.runtimes.length; ++i) {
+            if (list.runtimes[i].name === ret_obj.runtime && list.runtimes[i].available) {
+                runtime_found = true;
+                break;
+            }
+        }
+    }
+    
+    if (!runtime_found) {
+        console.error(util.format('Runtime "%s" could not be found, or is not available.', ret_obj.runtime));
+        process.exit(1);
+    }
+    
+    // now find the deviceid
+    var deviceid;
+    for (var i=0; i < list.devices.length; ++i) {
+        if (list.devices[i].runtime === ret_obj.runtime) { // found the runtime, now find the actual device matching devicename
+            var devlist = list.devices[i].devices;
+            for (var j=0; j < devlist.length; ++j) {
+                if (devlist[j].name === ret_obj.name) {
+                    ret_obj.id = devlist[j].id;
+                }
+            }
+        }
+    }
+    
+    if (!ret_obj.id) {
+        console.error(util.format('Device id for device "%s" and runtime "%s" could not be found, or is not available.', ret_obj.name, ret_obj.runtime));
+        process.exit(1);
+    }
+    
+    return ret_obj;
+}
 
 var command_lib = {
     
@@ -57,11 +152,10 @@ var command_lib = {
     
     launch : function(args) {
         var wait_for_debugger = false,
-            device = null,
-            app_identifier = null,
-            argv = [],
-            app_path = null,
-            info_plist_path = null;
+            app_identifier,
+            argv,
+            app_path,
+            info_plist_path;
 
         if (args.argv.remain.length < 2) {
             help();
@@ -76,31 +170,34 @@ var command_lib = {
         }
         
         bplist.parseFile(info_plist_path, function(err, obj) {
-          if (err) {
-              throw err;
-          }
-
-          app_identifier = obj[0].CFBundleIdentifier;
-          argv = args.args;
-        
-          // TODO: get the deviceid from --devicetypeid
-          // simctl.list needs to return the stdout data in JSON format
-          // --devicetypeid is a string in the form "devicetype, runtime_version"
-          // simctl.list({ devicetypes: true}) returns list of device types in the form of "devicename (devicetype)"
-          // from this, you can get the devicename from the devicetype
-          // simctl.list({ devices: true}) returns list of devices in the form of "devicename, (deviceid) (bootstate)". These are listed in sections "iOS runtime_version"
-          // From:
-          // --devicetypeid --> devicetype, runtime_version
-          // devicetype --> devicename
-          // devicename, runtime_version -> deviceid
           
-          //simctl.launch(wait_for_debugger, deviceid, app_identifier, argv)
+            if (err) {
+              throw err;
+            }
+
+            app_identifier = obj[0].CFBundleIdentifier;
+            argv = args.args || [];
+
+            // get the deviceid from --devicetypeid
+            // --devicetypeid is a string in the form "devicetype, runtime_version" (optional: runtime_version)
+            var device = processDeviceTypeId(args.devicetypeid);
+            
+            // so now we have the deviceid, we can proceed
+            simctl.extensions.start(device.name);
+            simctl.install(device.id, app_path);
+            simctl.launch(wait_for_debugger, device.id, app_identifier, argv);
         });
     },
     
     start : function(args) {
-        // TODO: launch without app
-        console.log("TODO: start");
+        var device = {};
+        try  {
+            device = processDeviceTypeId(args.devicetypeid);
+        } catch (e) {
+            // do nothing
+        }
+        
+        simctl.extensions.start(device.id);
     }
 }
 
