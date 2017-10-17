94129b7d30e3493e3db65fe1e3c6eeddea562656
aharbick@aharbick.com <aharbick@aharbick.com>
2017-10-17 17:34:58 -0400

    Don't filter devices anymore

--- src/lib.js
+++ src/lib.js
@@ -110,7 +110,7 @@ function findRuntimesGroupByDeviceProperty(list, deviceProperty, availableOnly) 
 function findAvailableRuntime(list, device_name) {
 
     var all_druntimes = findRuntimesGroupByDeviceProperty(list, 'name', true);
-    var druntime = all_druntimes[ filterDeviceName(device_name) ];
+    var druntime = all_druntimes[ device_name ];
     var runtime_found = druntime && druntime.length > 0;
 
     if (!runtime_found) {
@@ -199,7 +199,7 @@ function getDeviceFromDeviceTypeId(devicetypeid) {
         // found the runtime, now find the actual device matching devicename
         if (deviceGroup === ret_obj.runtime) {
             return list.devices[deviceGroup].some(function(device) {
-                if (filterDeviceName(device.name) === filterDeviceName(ret_obj.name)) {
+                if (device.name === ret_obj.name) {
                     ret_obj.id = device.udid;
                     return true;
                 }
@@ -255,15 +255,6 @@ function withInjectedEnvironmentVariablesToProcess(process, envVariables, action
 
     // restore old envs
     process.env = oldVariables;
-}
-
-// replace hyphens in iPad Pro name which differ in 'Device Types' and 'Devices'
-function filterDeviceName(deviceName) {
-    // replace hyphens in iPad Pro name which differ in 'Device Types' and 'Devices'
-    if (deviceName.indexOf('iPad Pro') === 0) {
-        return deviceName.replace(/\-/g, ' ').trim();
-    }
-    return deviceName;
 }
 
 function fixNameKey(array, mapping) {
@@ -349,7 +340,7 @@ var lib = {
         var name_id_map = {};
 
         list.devicetypes.forEach(function(device) {
-            name_id_map[ filterDeviceName(device.name) ] = device.identifier;
+            name_id_map[ device.name ] = device.identifier;
         });
 
         list = [];
@@ -366,9 +357,8 @@ var lib = {
 
         for (var deviceName in druntimes) {
             var runtimes = druntimes[ deviceName ];
-            var dname = filterDeviceName(deviceName);
 
-            if (!(dname in name_id_map)) {
+            if (!(deviceName in name_id_map)) {
                 continue;
             }
 
